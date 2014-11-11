89e60ece2f517fbf11786388ca50bf3dcb3b9642
fredfigueiredo <fredfigueiredo@gmail.com>
2014-11-11 10:31:56 +0100

    Improved readability.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,7 @@ clear
 # Download the OS X bootstrap scripts
 if test ! -d ~/.osx-bootstrap; then
   echo '### Preparing to setup your OS X...'
-  mkdir ~/.osx-bootstrap
-  cd ~/.osx-bootstrap
+  mkdir ~/.osx-bootstrap && cd $_
   curl -sLOk https://github.com/fredfigueiredo/osx-bootstrap/archive/master.zip
   tar -xf master.zip --strip 1
   rm master.zip
@@ -17,25 +16,25 @@ cd ~/.osx-bootstrap
 
 echo "### Setting up your OS X..."
 
-# Bootstrap parts separated by instruction, filename and whether or not it asks for confirmation
-# Example: 'Install APP instruction::FILENAME::0'
-# Confirmation: 0 => Do not ask; 1 => Ask
+# Bootstrap parts separated by filename, ask flag and instruction
+# Ask flag: 0 => Do not ask; 1 => Ask for confirmation
+# Example: 'FILENAME::0::Install APP instruction'
 parts=(
-  'NOINSTRUCTION::functions::0'
-  'Install Homebrew::brew::1'
-  'Install Homebrew Cask (Homebrew is required)::cask::1'
-  'Install applications listed in parts/cask-apps.sh (Homebrew Cask is required)::cask-apps::1'
+  'functions::0'
+  'brew::1::Install Homebrew'
+  'cask::1::Install Homebrew Cask (Homebrew is required)'
+  'cask-apps::1::Install apps listed in parts/cask-apps.sh (Cask is required)'
 )
 
 for part in "${parts[@]}"
 do
-  instruction=${part%%::*} # Removes filename and ask flag
-  tmp=${part#*::} # Removes instruction
-  file=${tmp%%::*} # Removes ask flag
-  ask=${part##*::} # Removes instruction and file name
+  inst=${part##*::} # Removes file name and ask flag
+  file=${part%%::*} # Removes ask flag and instruction
+  tmp=${part#*::}   # Removes file name
+  ask=${tmp%%::*}   # Removes instruction
 
   if test $ask == 1; then
-    confirm "$instruction?"
+    confirm "$inst?"
     if test $? == "0"; then
       source parts/$file.sh
     fi
