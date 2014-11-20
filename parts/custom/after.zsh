52bf14a494c5ca54b51470378ab0e9406870a8b7
fredfigueiredo <fredfigueiredo@gmail.com>
2014-11-20 10:54:20 +0100

    Added custom aliases.

--- parts/custom/after.zsh
+++ parts/custom/after.zsh
@@ -1,6 +1,9 @@
 # Set prompt
 prompt skwpcustom
 
+# Define your own aliases or override those provided by YADR
+alias hosts='sudo emacs /private/etc/hosts'
+
 # Add emacs-like key bindings to shell
 # Missing after YADR installation
 bindkey '^k' kill-line
