bda58b2995bcc23b63272c20f8c464d1fac016c6
fredfigueiredo <fredfigueiredo@gmail.com>
2014-11-21 10:32:23 +0100

    Fixed config zsh path.

--- parts/config-zsh.sh
+++ parts/config-zsh.sh
@@ -1,11 +1,11 @@
-custom_folder='~/.osx-bootstrap/parts/custom'
+custom_folder="$HOME/.osx-bootstrap/parts/custom"
 
 # Check if YADR is installed
 if test -d ~/.yadr; then
 
   info 'Customizing ZSH...'
-  cp ${custom_folder}/prompt_skwpcustom_setup ~/.zsh.prompts/prompt_skwpcustom_setup
-  cp ${custom_folder}/after.zsh ~/.zsh.after
+  cp "$custom_folder/prompt_skwpcustom_setup" ~/.zsh.prompts/prompt_skwpcustom_setup
+  cp "$custom_folder/after.zsh" ~/.zsh.after
 
 else
   log 'Skipping. YADR is not installed.'
