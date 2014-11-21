33a582d52077d286e38e20272d17ba126ac6f160
fredfigueiredo <fredfigueiredo@gmail.com>
2014-11-21 10:19:57 +0100

    Set emacs as favorite editor.

--- parts/custom/after.zsh
+++ parts/custom/after.zsh
@@ -4,6 +4,9 @@ prompt skwpcustom
 # Define your own aliases or override those provided by YADR
 alias hosts='sudo emacs /private/etc/hosts'
 
+# Set emacs has default editor
+export EDITOR=emacs
+
 # Add emacs-like key bindings to shell
 # Missing after YADR installation
 bindkey -e
