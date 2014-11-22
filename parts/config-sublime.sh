ebd90f55feae135e52eea3859db86d2835170436
fredfigueiredo <fredfigueiredo@gmail.com>
2014-11-22 23:46:35 +0100

    Opens sublime to create user folder.

--- parts/config-sublime.sh
+++ parts/config-sublime.sh
@@ -4,7 +4,15 @@ sublime_templates="$HOME/.osx-bootstrap/templates/sublime"
 sublime_user_folder="$HOME/Library/Application Support/Sublime Text 2"
 sublime_bin_folder='/Applications/Sublime Text 2.app/Contents/SharedSupport/bin'
 
-if test -d "$sublime_user_folder" && test -d "$sublime_bin_folder"; then
+if test -d "$sublime_bin_folder"; then
+
+  # Open and close sublime to create user folder if needed
+  if test -d "$sublime_user_folder"; then
+    "$sublime_bin_folder/subl"
+  fi
+
+  # Closes any open instance of sublime
+  kill $(ps aux | grep 'sublime' | awk '{print $2}') 2>/dev/null
 
   info 'Customizing Sublime Text2...'
 
@@ -23,10 +31,9 @@ if test -d "$sublime_user_folder" && test -d "$sublime_bin_folder"; then
   # Set alias to Sublime 2
   if test -d ~/.yadr; then
     ln -sf "$sublime_bin_folder/subl" ~/.yadr/bin
+  else
+    ln -sf "$sublime_bin_folder/subl" /usr/local/bin/subl
   fi
-
-  log 'Restart Sublime Text 2 for changes to take effect.'
-  pause
 
 else
   log 'Sublime Text 2 is not installed. Skipping.'
