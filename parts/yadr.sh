4931b9ff5d76519a59a52def6a2ee5c41abfabfa
fredfigueiredo <fredfigueiredo@gmail.com>
2015-04-16 23:28:21 +0200

    Removed yard confs from module - missing.

--- parts/yadr.sh
+++ parts/yadr.sh
@@ -1,16 +1,10 @@
 # YADR: http://skwp.github.io/dotfiles/
 
-custom_folder='~/.osx-bootstrap/parts/custom'
-
 # Check if YADR is already installed
 if test ! -d ~/.yadr; then
 
   info 'Installing YADR...'
   sh -c "`curl -fsSL https://raw.github.com/skwp/dotfiles/master/install.sh`"
-
-  info 'Adding customizations...'
-  cp ${custom_folder}/prompt_skwpcustom_setup ~/.zsh.prompts/prompt_skwpcustom_setup
-  cp ${custom_folder}/after.zsh ~/.zsh.after
 
 else
   log 'YADR already installed.'
